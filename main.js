75e89767bfe8519d0118eca758269077b3d89c77
Connor Redmond <connred@icloud.com>
2017-01-05 12:00:30 -0600

    Html updates

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+if(window.location.hostname === '127.0.0.1'){
+    window.location = 'http://localhost:1898';
+}
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
