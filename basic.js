874d80d8ca67a91b82b11af008182f5c3f719c8f
Connor Redmond <connred@icloud.com>
2017-01-09 11:18:18 -0600

    Restart

--- basic.js
+++ basic.js
@@ -1,16 +1,14 @@
 /*if (window.location.hostname === '127.0.0.1') {
     window.location = 'http://localhost:1898';
 }*/
-var socket = io.connect();
-
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    var username = profile.getName();
+    username = profile.getName();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
-    $('#name').append('<p>' + profile.getName() + '</p>');
-    $('#email').append('<p>' + 'Email' + ':' + profile.getEmail() + '</p>');
-    init();
+    $('#name').html('<p>' + profile.getName() + '</p>');
+    $('#email').html('<p>' + 'Email' + ':' + profile.getEmail() + '</p>');
+    $('.g-signin2').hide();
 }
